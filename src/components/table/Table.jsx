20f668e25f0dc719b2d4f4d122e93c92d51243c4
proshantosaha <proshantoll71@gmail.com>
2023-06-05 16:12:33 +0600

    add search box na d filter

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,11 +1,31 @@
 import React, { useState } from 'react'
 
 const Table = ({contacts}) =>{
-    const [filter,setFilter] = useState('All')
+    const [filter,setFilter] = useState('All');
+
+    const [searchTerm, setSearchTerm] = useState('');
 
     const handleChange =(e)=>{
-        setFilter(e.target.value)
+        setFilter(e.target.value);
     }
+
+    const searchCB = (contact)=>
+      contact.name.includes(searchTerm)|| contact.email.includes(searchTerm);
+    
+
+    let filterdContacts = [];
+    if (filter === 'All'){
+        filterdContacts = searchTerm ? contacts.filter(searchCB):contacts;
+    }else{
+        filterdContacts = contacts.filter(contact=>contact.group ===filter && searchCB(contact)) ;
+    }
+
+    // if(searchTerm){
+    //     filterdContacts = contacts.filter(contact=>contact.name.includes(searchTerm)||
+    //     contact.email.includes(searchTerm));
+    // }
+
+
 
     return (
         <>
@@ -14,31 +34,36 @@ const Table = ({contacts}) =>{
                 Filters:
                 <select onChange={handleChange}>
                     <option value='All'>All</option>
-                    <option value='None'>None</option>
+                    <option value=''>None</option>
                     <option value='Home'>Home</option>
                     <option value='Office'>Office</option>
                 </select>
+                <input 
+                type='search' 
+                placeholder='search ' 
+                value={searchTerm}
+                onChange={e=>setSearchTerm(e.target.value)}
+                />  
+                <table>
+                    <thead>
+                        <tr>
+                        <th>name</th>
+                        <th>email</th>
+                        <th>Group</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                    {filterdContacts.map((contact,index)=>(
+                        <tr key={index}>
+                        <td>{contact.name}</td>
+                        <td>{contact.email}</td>
+                        <td>{contact.group}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
-
-            <table>
-            <thead>
-                <tr>
-                <th>name</th>
-                <th>email</th>
-                <th>Group</th>
-                </tr>
-            </thead>
-
-            <tbody>
-            {contacts.map((contact,index)=>(
-                <tr key={index}>
-                <td>{contact.name}</td>
-                <td>{contact.email}</td>
-                <td>{contact.group}</td>
-                </tr>
-                ))}
-            </tbody>
-            </table>
         </>
     )
   
